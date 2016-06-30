19cce09b34053ced38e738e5c27b6b3f9b867222
Maria Luciene <marialucienefelix@gmail.com>
2016-06-30 00:39:56 -0300

    Organizando campos dos históricos

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 find ./ -name "migrations" | xargs rm -rf
 rm db.sqlite3
-python3.5 manage.py makemigrations app
-python3.5 manage.py migrate
-python3.5 manage.py createsuperuser
+python manage.py makemigrations app
+python manage.py migrate
+python manage.py createsuperuser
